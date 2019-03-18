377144462c6cbab1025248abec4c5e40b8ab7441
Denis <denis.gerashchenko@testmatick.com>
2019-03-18 19:19:52 +0200

    index view

--- spacer/settings.py
+++ spacer/settings.py
@@ -112,3 +112,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
