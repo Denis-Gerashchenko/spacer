7345f0465ea402e522b591e0c93fd75a261a6b6e
Denis-Gerashchenko <46624843+Denis-Gerashchenko@users.noreply.github.com>
2019-03-18 16:16:22 +0200

    Update settings.py

--- spacer/settings.py
+++ spacer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'on94j+u@v22&_!5*_=#tb&4)^x*e$c)bi$^t#$6pufbm4@y1hn'
+SECRET_KEY = 'onion'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
