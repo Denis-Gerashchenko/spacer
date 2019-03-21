e294e66b05ccc95adc57a0e7f3ce83a5dce844c4
Denis <denis.gerashchenko@testmatick.com>
2019-03-21 16:52:13 +0200

    Норм

--- spaces/views.py
+++ spaces/views.py
@@ -1,13 +1,17 @@
-from django.views import generic
+from django.views import View
+from django.shortcuts import render
 from .models import Space
 
 # Create your views here.
-class Index_view(generic.DetailView):
+class Index_view(View):
     template_name = 'index.html'
-    context_object_name = 'all_spaces'
 
-    def get_queryset(self):
-        return Space.objects.all()
+    def get(self, request, *args, **kwargs):
+        spaces = Space.objects.all()
+        context = {
+            'spaces': spaces
+        }
+        return render(request, self.template_name, context)
 
 def space_view(request):
     return render(request, 'single-post-1.html', {})
